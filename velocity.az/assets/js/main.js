c93c6488a6d1bd3cdca6c6a06e4b6200cd9df175
ilhambagirov <ilhambb@code.edu.az>
2021-08-07 01:32:49 -0700

    (responsive first slider is done)

--- velocity.az/assets/js/main.js
+++ velocity.az/assets/js/main.js
@@ -13,3 +13,24 @@ otherLangs.forEach(otherlang=>{
         currentspan.innerText=e.target.innerText
     })
 })
+
+
+///slider dots
+
+$( function() {
+  
+    var $mainslider = $('.gallery').flickity({
+      pageDots: true
+    });
+  
+    var flickity = $mainslider.data('flickity');
+    
+    var $galleryNav = $('.flickity-page-dots');
+    var $galleryNavItems = $galleryNav.find('.flickity-page-dots .dot');
+    
+    $mainslider.on( 'select.flickity', function() {
+      $galleryNav.find('.is-selected').removeClass('is-selected');
+      $galleryNavItems.eq( flickity.selectedIndex ).addClass('is-selected');
+    });
+  });
+  
