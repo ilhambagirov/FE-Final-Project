3429350f3448772e829739c1210b710cb3359087
ilhambagirov <ilhambb@code.edu.az>
2021-08-08 03:51:28 -0700

    (checkbox fixed and calculating shipping added by js)

--- velocity.az/assets/js/main.js
+++ velocity.az/assets/js/main.js
@@ -35,23 +35,61 @@ $( function() {
   });
 
   //calculator slider
+ 
 
+(function range(){
+  
   let rangevalues=document.querySelectorAll('#rangevalue')
   let rangesliders=document.querySelectorAll('#rangeslider')
   rangesliders.forEach(rangeslider=>{
-   
-
       rangeslider.addEventListener("input", function(e) {
         rangevalues.forEach(rangevalue=>{
         if (rangeslider.getAttribute('data-source')===rangevalue.getAttribute('data-target')) {
           rangevalue.value = rangeslider.value;
         }
-      
       });
     })
    
   })
-  
+})()
+
+
+//checkbox checker
+
+let service=document.querySelector('.services')
+let labels=service.querySelectorAll('label')
+let inputs=service.querySelectorAll('input')
+let ticks=service.querySelectorAll('.tickcustom')
+   inputs.forEach(input=>{
+    input.addEventListener('click',e=>{
+      ticks.forEach(tick=>{
+      if (tick.getAttribute('data-source')===input.getAttribute('data-target')) {
+         input.classList.toggle('checkboxback')
+         tick.classList.toggle('tickactive')
+      }
+})
+  })
+})
+
+//calculator
+let calculate=document.querySelector('.customOperate')
+calculate.addEventListener('click',e=>{
+  calc()
+})
+function calc(){
+let a=Number(document.querySelector('input[data-target=en]').value)
+let b=Number(document.querySelector('input[data-target=height]').value)
+let c=Number(document.querySelector('input[data-target=width]').value)
+let d=Number(document.querySelector('input[data-target=kg]').value)
+let dollar=document.querySelector('#calc-total')
+let manat=document.querySelector('.manat')
+let total=(a+b+c+d)/2
+dollar.innerText=total
+manat.innerText=total*1.7
+
+}
+
+
 
 
 
